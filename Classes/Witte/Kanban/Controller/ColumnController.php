7fc03ed94e49bfe41d32bd26754e57165569a3eb
Matthias Witte <wittematze@gmail.com>
2012-12-12 21:51:08 +0100

    bugfix for creating subColumns, refactored templates, removed needless templates

--- Classes/Witte/Kanban/Controller/ColumnController.php
+++ Classes/Witte/Kanban/Controller/ColumnController.php
@@ -10,7 +10,6 @@ use TYPO3\Flow\Annotations as Flow;
 
 use TYPO3\Flow\Mvc\Controller\ActionController;
 use \Witte\Kanban\Domain\Model\Board;
-use \Witte\Kanban\Domain\Model\SuperiorColumn;
 use \Witte\Kanban\Domain\Model\Column;
 use \Witte\Kanban\Domain\Model\Ticket;
 
